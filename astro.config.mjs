7de3ce32249df107506e9c3c44f8a6b3a2b424ea
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2025-03-10 21:42:44 +0000

    switch to prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,9 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
