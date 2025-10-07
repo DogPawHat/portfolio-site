d5821eec6d89742c0ba6f6c33f85bda8d6a35718
Ciarán Curley <ciaran@alludium.ai>
2025-10-07 12:58:29 +0100

    svg fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,6 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
-  experimental: {
-    svg: true,
-  },
   vite: {
     plugins: [tailwindcss()],
   },
